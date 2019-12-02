f61b6bc405567148ccc68f7135f98c28da2185c9
safronman <safronmanbox@gmail.com>
2019-12-02 09:27:58 +0300

    1. Create components, todo-services and base layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodolistComponent } from './components/todolist/todolist.component';
+import { TaskComponent } from './components/task/task.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodolistComponent,
+    TaskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
