4b4968f947c505ea3b1d8040a95ba3d4fd6c95bd
safronman <safronmanbox@gmail.com>
2019-12-03 13:24:15 +0300

    14. Refactoring todolists service

--- src/app/services/todolist.service.ts
+++ src/app/services/todolist.service.ts
@@ -7,27 +7,31 @@ import {delay, map} from 'rxjs/operators';
 @Injectable({providedIn: 'root'})
 export class TodolistService {
 
+
+    baseUrl = 'https://social-network.samuraijs.com/api/1.0/todo-lists';
+
+    headers = new HttpHeaders(
+        {
+            'API-KEY': '794181ab-6d62-4cfb-bc9f-d539dfac55f1'
+        }
+    );
+
+    options = {
+        withCredentials: true,
+        headers: this.headers
+    };
+
+
     constructor(private http: HttpClient) {
     }
 
     getTodolists(): Observable<Todolist[]> {
-        return this.http.get<Todolist[]>('https://social-network.samuraijs.com/api/1.0/todo-lists',
-            {
-                withCredentials: true,
-                headers: new HttpHeaders({
-                    'API-KEY': '794181ab-6d62-4cfb-bc9f-d539dfac55f1'
-                })
-            })
+        return this.http.get<Todolist[]>(this.baseUrl, this.options)
             .pipe(delay(1500));
     }
 
     createTodolist(title): Observable<Todolist> {
-        return this.http.post<Todolist>('https://social-network.samuraijs.com/api/1.0/todo-lists', {title}, {
-            withCredentials: true,
-            headers: new HttpHeaders({
-                'API-KEY': '794181ab-6d62-4cfb-bc9f-d539dfac55f1'
-            })
-        })
+        return this.http.post<Todolist>(this.baseUrl, {title}, this.options)
             .pipe(
                 map((res: any) => {
                     return res.data.item;
@@ -36,22 +40,12 @@ export class TodolistService {
     }
 
     deleteTodolist(todoId) {
-        return this.http.delete<void>(`https://social-network.samuraijs.com/api/1.0/todo-lists/${todoId}`, {
-            withCredentials: true,
-            headers: new HttpHeaders({
-                'API-KEY': '794181ab-6d62-4cfb-bc9f-d539dfac55f1'
-            })
-        });
+        return this.http.delete<void>(`${this.baseUrl}/${todoId}`, this.options);
     }
 
 
     getTasks(todolistId): Observable<Task[]> {
-        return this.http.get<Task[]>(`https://social-network.samuraijs.com/api/1.0/todo-lists/${todolistId}/tasks`, {
-            withCredentials: true,
-            headers: new HttpHeaders({
-                'API-KEY': '794181ab-6d62-4cfb-bc9f-d539dfac55f1'
-            })
-        })
+        return this.http.get<Task[]>(`${this.baseUrl}/${todolistId}/tasks`, this.options)
             .pipe(
                 delay(1500),
                 map((res: any) => {
@@ -61,12 +55,7 @@ export class TodolistService {
     }
 
     createTask(todolistId, title): Observable<Task> {
-        return this.http.post<Task>(`https://social-network.samuraijs.com/api/1.0/todo-lists/${todolistId}/tasks`, {title}, {
-            withCredentials: true,
-            headers: new HttpHeaders({
-                'API-KEY': '794181ab-6d62-4cfb-bc9f-d539dfac55f1'
-            })
-        })
+        return this.http.post<Task>(`${this.baseUrl}/${todolistId}/tasks`, {title}, this.options)
             .pipe(
                 map((res: any) => {
                     return res.data.item;
@@ -75,30 +64,15 @@ export class TodolistService {
     }
 
     deleteTask(taskId) {
-        return this.http.delete<void>(`https://social-network.samuraijs.com/api/1.0/todo-lists/tasks/${taskId}`, {
-            withCredentials: true,
-            headers: new HttpHeaders({
-                'API-KEY': '794181ab-6d62-4cfb-bc9f-d539dfac55f1'
-            })
-        });
+        return this.http.delete<void>(`${this.baseUrl}/tasks/${taskId}`, this.options);
     }
 
     updateTodoTitle(todoId, title) {
-        return this.http.put(`https://social-network.samuraijs.com/api/1.0/todo-lists/${todoId}`, {title}, {
-            withCredentials: true,
-            headers: new HttpHeaders({
-                'API-KEY': '794181ab-6d62-4cfb-bc9f-d539dfac55f1'
-            })
-        });
+        return this.http.put(`${this.baseUrl}/${todoId}`, {title}, this.options);
     }
 
     updateTask(task): Observable<Task> {
-        return this.http.put(`https://social-network.samuraijs.com/api/1.0/todo-lists/tasks`, task, {
-            withCredentials: true,
-            headers: new HttpHeaders({
-                'API-KEY': '794181ab-6d62-4cfb-bc9f-d539dfac55f1'
-            })
-        })
+        return this.http.put(`${this.baseUrl}/tasks`, task, this.options)
             .pipe(
                 map((res: any) => {
                     return res.data.item;
